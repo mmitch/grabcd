7f4d5b757ffcb6ddc8a2f97614fa48575aaa9f90
mitch <mitch>
2004-06-04 22:00:18 +0000

    read first data

--- scancd.pl
+++ scancd.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl -w
-# $Id: scancd.pl,v 1.2 2004-06-04 21:39:48 mitch Exp $
+# $Id: scancd.pl,v 1.3 2004-06-04 22:00:18 mitch Exp $
 #
 # 2004 (c) by Christian Garbs <mitch@cgarbs.de>
 # Licensed under GNU GPL
@@ -7,7 +7,10 @@
 use strict;
 use Audio::CD;
 
+# globals
 my ($keep_year, $keep_artist, $keep_title, $keep_version) = ( 1, 1, 0, 0 );
+my $file = '/tmp/cdinfo';
+
 
 my $args = join '', @ARGV;
 $args =~ tr/A-Z/a-z/;
@@ -29,3 +32,21 @@ while ( $args =~ /(.)/g ) {
 	exit;
     }
 }
+
+my $cd = Audio::CD->init;
+die "could not initialize Audio::CD\n" unless defined $cd;
+
+my $stat = $cd->close;
+$stat = $cd->stat;
+die "no cd detected\n" unless $stat->present;
+
+my $cddb = $cd->cddb;
+my $discid = $cddb->discid;
+
+print "discid=[$discid], track_count=[".$stat->total_tracks."]\n";
+
+open CDINFO, '>', $file or die "can't open `$file': $!\n";
+
+print CDINFO "DISCID=$discid\n";
+
+close CDINFO or die "can't close `$file': $!\n";
