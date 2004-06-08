8f07c218818b9b23d78d9aebfcc467a9bbb1e4bf
mitch <mitch>
2004-06-08 20:26:19 +0000

    activate copy of cdinfo

--- scancd.pl
+++ scancd.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl -w
-# $Id: scancd.pl,v 1.10 2004-06-08 19:57:11 mitch Exp $
+# $Id: scancd.pl,v 1.11 2004-06-08 20:26:19 mitch Exp $
 #
 # 2004 (c) by Christian Garbs <mitch@cgarbs.de>
 # Licensed under GNU GPL
@@ -53,7 +53,7 @@ print CDINFO "DISCID=$discid\n";
 
 use Term::ReadLine;
 my ($artist, $album, $path, $title, $version, $year);
-my $term = new Term::ReadLine 'scancd $Id: scancd.pl,v 1.10 2004-06-08 19:57:11 mitch Exp $';
+my $term = new Term::ReadLine 'scancd $Id: scancd.pl,v 1.11 2004-06-08 20:26:19 mitch Exp $';
 $|++;
 
 $artist = $term->readline("Artist  : ");
@@ -109,4 +109,4 @@ use File::Copy;
 
 $path =~ s,/,___,;
 $path =~ s/ /\\ /g;
-system('echo', 'scp', $file, "$remote/$path");
+system('scp', $file, "$remote/$path");
