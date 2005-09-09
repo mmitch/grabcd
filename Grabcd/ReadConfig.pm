9d0d39f6b0e5bcac4a14a676fc0e1250953359c8
mitch <mitch>
2005-09-09 20:09:53 +0000

    initial source import

---
+++ Grabcd/ReadConfig.pm
@@ -0,0 +1,56 @@
+#!/usr/bin/perl -w
+# $Id: ReadConfig.pm,v 1.1 2005-09-09 20:09:53 mitch Exp $
+#
+# 2005 (c) by Christian Garbs <mitch@cgarbs.de>
+# utility function to read a simple config file
+# if not all keywords are found, an error is raised
+# $filename is searched for as "~/.$filename" and "/etc/$filename.conf"
+# file format is:
+# # comment
+# KEY=value
+
+use strict;
+
+package Grabcd::ReadConfig;
+
+sub read_config($@)
+# $_[0]    = Configuration file name
+# $_[1..n] = must-have configuration keys
+# result is an hash of the configuration
+{
+    my $filename = shift;
+    my @keys = @_;
+
+    my $result = {};
+
+    my $file = "$ENV{HOME}/.$filename";
+
+    if (! -r $file) {
+	$file = "/etc/$filename.conf";
+    }
+
+    die "ERROR configuration file <$file> does not exist.\n" unless -e $file;
+    die "ERROR configuration file <$file> is not readable.\n" unless -r _;
+
+    open CONF, '<', $file or die "can't open <$file>: $!\n";
+    while (my $line = <CONF>) {
+	chomp $line;
+	next if $line =~ /^\s*#/;
+	next if $line =~ /^\s*$/;
+	if ($line =~ /^\s*([A-Z]+)=(.+)\s*$/) {
+	    $result->{uc $1} = $2;
+	} else {
+	    warn "unparseable line $. in configuration file\n";
+	}
+    }
+    close CONF or die "can't close <$file>: $!\n";
+
+    foreach my $key (@keys) {
+	$key = uc $key;
+	die "configuration option $key is not set.\n" unless exists $result->{$key};
+    }
+
+    return $result;
+}
+
+1;
